a032cd445db0cfc08d56101f72462f3ed9c64072
coocoder <delaneyb@fullsixuk.com>
2015-10-22 11:36:37 +0100

    create an example

---
+++ examples/data.rb
@@ -0,0 +1,24 @@
+require "rubygems"
+require "cleanr"
+
+
+db = Cleanr::Database.new
+
+db.table :cms_users do
+  field :email, unique: true
+  field :encrypted_password, type: :password
+  field :name
+end
+
+db.table :users do
+  field :email, uniq: true
+  field :encrypted_password, type: :password
+  field :reset_password_token, type: :password_token
+  field :first_name
+  field :last_name
+  field :display_name
+  field :nectar_card_number,type: :number
+  field :confirmation_token,type: :number
+  field :unconfirmed_email,type: :email
+  field :unlock_token,type: :number
+end
